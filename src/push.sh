dc3895f43f8cb4e5aa8d1c5f160d3d37d57737be
silviu <silviu>
2024-01-30 21:15:11 +0100

    feat(feature) -  work on committing changed files

--- src/push.sh
+++ src/push.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -e
 
+
+echo "-- push      --"
 echo "----"
 echo "${GITHUB_ACTOR}"
+echo "${secrets.GITHUB_TOKEN}"
+git config --global user.name ${GITHUB_ACTOR}
+git config --global user.email ${GITHUB_ACTOR}@users.noreply.github.com
+git add README.md
+git commit -m "coverage update"
+git push
+echo "-- push done --"
 echo "----"
