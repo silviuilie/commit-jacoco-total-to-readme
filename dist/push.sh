39a9f2a8b180dd5b6f721ed33cd229eafa372aa6
silviu <silviu>
2024-02-02 21:26:38 +0100

    feat(feature) -  fileName print

--- dist/push.sh
+++ dist/push.sh
@@ -7,7 +7,7 @@ echo "----"
 echo "${GITHUB_ACTOR}"
 git config --global user.name ${GITHUB_ACTOR}
 git config --global user.email ${GITHUB_ACTOR}@users.noreply.github.com
-git add README.md
+git add ./doc/badges/coverage.svg
 git commit -m "coverage update"
 git push
 echo "-- push done --"
